bb31cd8c5b008c16b5a42acab40341134d9769a1
kseed <kseed@student.21-school.ru>
2021-09-13 23:17:28 +0300

    edit mysql sh

--- srcs/mysql/mysql_conf.sh
+++ srcs/mysql/mysql_conf.sh
@@ -1,6 +1,6 @@
 service mysql start
-mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
-mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS'"
-mysql -e "GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'%'"
-mysql -e "FLUSH PRIVILEGES;"
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+mysql -u root -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS'"
+mysql -u root -e "GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'%'"
+mysql -u root -e "FLUSH PRIVILEGES;"
 mysqladmin -e root password $ROOT_PASS
