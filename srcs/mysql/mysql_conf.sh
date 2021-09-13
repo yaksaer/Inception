1fa1cbe742ac1ace258800fd563f6bd5ab13c5d3
kseed <kseed@student.21-school.ru>
2021-09-13 23:46:39 +0300

    working with mysql sh

--- srcs/mysql/mysql_conf.sh
+++ srcs/mysql/mysql_conf.sh
@@ -1,6 +1,17 @@
-service mysql start
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
-mysql -u root -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS'"
-mysql -u root -e "GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'%'"
-mysql -u root -e "FLUSH PRIVILEGES;"
-mysqladmin -u root password $ROOT_PASS
+sed -i 's/bind-ad/#bind-ad/g' /etc/mysql/mariadb.conf.d/50-server.conf
+sed -i 's/#port/port /g' /etc/mysql/mariadb.conf.d/50-server.conf
+chown -R mysql:mysql /var/lib/mysql
+if [ ! -d var/lib/mysql/wp ]; then
+	chmod 744 /var/run/mysqld/mysqld.sock
+	service mysql start
+	mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+	mysql -u root -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS'"
+	mysql -u root -e "GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'%'"
+	mysql -u root -e "FLUSH PRIVILEGES;"
+	mysqladmin -u root password $ROOT_PASS
+fi
+if [! -d /var/run/mysqld ]; then
+	mkdir /var/run/mysqld
+	touch /var/run/mysqld/mysqld.pid
+	mkinfo /var/run/mysqld/mysqld.sock
+fi
