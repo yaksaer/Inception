2ec81180d26628ad1b31430c69d5e38a6ad1e5b5
kseed <kseed@student.21-school.ru>
2021-09-13 16:32:43 +0300

    improve dir

--- srcs/wordpress/wp_conf.sh
+++ srcs/wordpress/wp_conf.sh
@@ -1,8 +1,8 @@
 if [ ! -f /var/www/html/wp-config.php ]; then
 	wget -c https://wordpress.org/latest.tar.gz
 	tar -xzf latest.tar.gz
-	mv wordpress ../var/www/kseed/wordpress
+	mv wordpress ../var/www/html
 	rm -rf latest.tar.gz
-	cp ../files/wp-config.php ../var/www/kseed/wordpress
+	cp ../files/wp-config.php ../var/www/html
 fi
 exec "$@"
