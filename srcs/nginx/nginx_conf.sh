dcc4b817452ebdf97e2a7db776135a1de3e21d19
kseed <kseed@student.21-school.ru>
2021-09-15 19:22:18 +0300

    --""--""

--- srcs/nginx/nginx_conf.sh
+++ srcs/nginx/nginx_conf.sh
@@ -3,6 +3,6 @@ if [ ! -d /etc/ssl/private/keyout.key ]; then
 	openssl req -x509 -nodes -days 10 -subj "/C=RU/ST=Russia/L=Moscow/O=School21/OU=21Moscow/CN=kseed" -newkey  rsa:2048 \
     -keyout /etc/ssl/keyout.key \
     -out /etc/ssl/sertif.crt
-    mv ./files/config /etc/nginx/
+    mv ./config /etc/nginx/
 fi
 exec "$@"
