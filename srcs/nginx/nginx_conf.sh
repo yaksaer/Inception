1c15ea87282dc73f4515eea8a1c57c5fdb648406
kseed <kseed@student.21-school.ru>
2021-09-15 21:57:14 +0300

    rename nginx config

--- srcs/nginx/nginx_conf.sh
+++ srcs/nginx/nginx_conf.sh
@@ -3,7 +3,7 @@ if [ ! -d /etc/ssl/private/keyout.key ]; then
 	openssl req -x509 -nodes -days 10 -subj "/C=RU/ST=Russia/L=Moscow/O=School21/OU=21Moscow/CN=kseed" -newkey  rsa:2048 \
     -keyout /etc/ssl/keyout.key \
     -out /etc/ssl/sertif.crt
-    mv ./config /etc/nginx/
+    mv ./nginx.conf /etc/nginx/
 fi
-chmod 744 /etc/nginx/config
+chmod 744 /etc/nginx/nginx.conf
 exec "$@"
