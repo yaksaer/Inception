0e6262f2bb89592c255fa1ec116e5ec97a34749a
kseed <kseed@student.21-school.ru>
2021-09-15 21:10:29 +0300

    improve files

--- srcs/mysql/mysql_conf.sh
+++ srcs/mysql/mysql_conf.sh
@@ -3,13 +3,14 @@ sed -i 's/bind-ad/#bind-ad/g' /etc/mysql/mariadb.conf.d/50-server.cnf
 sed -i 's/#port /port /g' /etc/mysql/mariadb.conf.d/50-server.cnf
 chown -R mysql:mysql /var/lib/mysql
 if [ ! -d var/lib/mysql/wp ]; then
-	chmod 744 /var/run/mysqld/mysqld.sock
 	service mysql start
+	chmod 744 /var/run/mysqld/mysqld.sock
 	mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
 	mysql -u root -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS'"
 	mysql -u root -e "GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'%'"
 	mysql -u root -e "FLUSH PRIVILEGES"
 	mysqladmin -u root password $ROOT_PASS
+	service mysql stop
 fi
 if [ ! -d /var/run/mysqld ]; then
 	mkdir /var/run/mysqld
