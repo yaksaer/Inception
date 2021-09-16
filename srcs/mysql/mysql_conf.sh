12cc415a8ef6e150af67c683e42eeee0f608458d
kseed <kseed@student.21-school.ru>
2021-09-16 16:39:35 +0300

    remove root from compose

--- srcs/mysql/mysql_conf.sh
+++ srcs/mysql/mysql_conf.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-sed -i 's/bind-ad/#bind-ad/g' /etc/mysql/mariadb.conf.d/50-server.cnf
-sed -i 's/#port/port /g' /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i 's/bind-addr/\#bind-addr/g' /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i 's/#port /port /g' /etc/mysql/mariadb.conf.d/50-server.cnf
 chown -R mysql:mysql /var/lib/mysql
 if [ ! -d var/lib/mysql/wp ]; then
 	service mysql start
@@ -12,7 +12,7 @@ if [ ! -d var/lib/mysql/wp ]; then
 	mysqladmin -u root password $ROOT_PASS
 	service mysql stop
 else
-	mkdir /var/run/mysqld
+	mkdir -p /var/run/mysqld
 	touch /var/run/mysqld/mysqld.pid
 	mkfifo /var/run/mysqld/mysqld.sock
 fi
