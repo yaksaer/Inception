4a4915cd14a4492ad634f6da1652ba708b954014
kseed <kseed@student.21-school.ru>
2021-09-16 19:42:13 +0300

    add nginx conf in sites-available

--- srcs/nginx/nginx_conf.sh
+++ srcs/nginx/nginx_conf.sh
@@ -3,7 +3,8 @@ if [ ! -d /etc/ssl/private/keyout.key ]; then
 	openssl req -x509 -nodes -days 10 -subj "/C=RU/ST=Russia/L=Moscow/O=School21/OU=21Moscow/CN=kseed" -newkey  rsa:2048 \
     -keyout /etc/ssl/keyout.key \
     -out /etc/ssl/sertif.crt
-    mv ./nginx.conf /etc/nginx/
 fi
-chmod 744 /etc/nginx/nginx.conf
+chmod 744 ./nginx.conf
+cp ./nginx.conf /etc/nginx/sites-available
+ln -s /etc/nginx/sites-available/nginx.conf /etc/nginx/sites-enabled
 exec "$@"
